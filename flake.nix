dfc1ef771575f3878b3d2edef98043ff45beca9c
Jörg Thalheim <joerg@thalheim.io>
2022-01-27 04:43:28 +0100

    simplify flake a bit
    I don't see yet any use case beyond using this on any other cpu than
    x86_64-linux...,  maybe in future when risc64 takes over :)
    
    So let's keep it simple for now

--- flake.nix
+++ flake.nix
@@ -3,20 +3,9 @@
   # pin this to unstable
   # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          aarch64imgs = pkgs.callPackage ./. { };
-        in {
-          inherit (aarch64imgs)
-            aarch64Image rock64 rockPro64 roc-pc-rk3399 pinebookPro;
-        });
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux = import ./. {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
     };
+  };
 }
