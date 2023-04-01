acb24b9cf22ae9bc7c7f6157575975a8f6db9a0b
Jörg Thalheim <joerg@thalheim.io>
2023-04-01 07:04:29 +0000

    add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Build NixOS images for various ARM single computer boards";
   # pin this to unstable
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux = import ./. {
