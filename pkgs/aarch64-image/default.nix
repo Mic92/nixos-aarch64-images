2166cfc29b3d8d2f60548c3e5bd8b09f7dafe45e
Tom Vincent <git@tlvince.com>
2024-12-12 21:19:42 +0000

    Bump image

--- pkgs/aarch64-image/default.nix
+++ pkgs/aarch64-image/default.nix
@@ -4,9 +4,9 @@ let
     # unfortunally there is no easy way right now to reproduce the same evaluation
     # as hydra, since `pkgs.path` is embedded in the binary
     # To get a new url use:
-    # $ curl -s -L -I -o /dev/null -w '%{url_effective}' "https://hydra.nixos.org/job/nixos/release-20.03/nixos.sd_image.aarch64-linux/latest/download/1"
-    url = "https://hydra.nixos.org/build/269682674/download/1/nixos-sd-image-24.05.3914.c3d4ac725177-aarch64-linux.img.zst";
-    sha256 = "0g11hpsl8sw44vi7f6pmhxi1x59v61mj2vcpv5cz3j98r58w13y6";
+    # $ curl -s -L -I -o /dev/null -w '%{url_effective}' "https://hydra.nixos.org/job/nixos/trunk-combined/nixos.sd_image_new_kernel_no_zfs.aarch64-linux/latest/download/1"
+    url = "https://hydra.nixos.org/build/282058919/download/1/nixos-sd-image-25.05beta720697.5d67ea6b4b63-aarch64-linux.img.zst";
+    sha256 = "5827626d41174efca81b045ab1b7273c70b9dc41dd8cc997ba266819d904ce30";
   }).overrideAttrs (final: prev: {
     __structuredAttrs = true;
     unsafeDiscardReferences.out = true;
