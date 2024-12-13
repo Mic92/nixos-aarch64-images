544fc9ebf89173e639650c1f501d51832c93458a
Jörg Thalheim <joerg@thalheim.io>
2024-12-13 10:40:53 +0100

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "Build NixOS images for various ARM single computer boards";
   # pin this to unstable
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?shallow=1&ref=nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs, ... }: {
     packages.x86_64-linux = import ./. {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     };
